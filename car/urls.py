903d7f6e9ade75d4f04db906b429b85ade1e73b0
Yuliia <juliajvu@gmail.com>
2018-10-17 17:46:44 +0300

    creating api for carslist

--- car/urls.py
+++ car/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from car import views
 from car.views import *
 
 urlpatterns = format_suffix_patterns([
+    path('carslist/', views.CarList.as_view()),
 ])
--- car/views.py
+++ car/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import CityCar, Car, TransmissionCar, ModelCar, BrandCar
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CarSerializer
 # Create your views here.
+
+
+class CarList(APIView):
+
+    def get(self, request):
+        carslist = Car.objects.all()
+        serializer = CarSerializer(carslist, many=True)
+        return Response(serializer.data)
+
