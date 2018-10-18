f1ecec97f89b4e594872eec1e0f03659b4474857
Artemion <artemdanilenko1997@gmail.com>
2018-10-18 19:20:02 +0300

    new views

--- car/urls.py
+++ car/urls.py
@@ -3,6 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from car.views import CarDetail, CarList
 
 urlpatterns = format_suffix_patterns([
-    path('car/<int:pk>/', CarDetail.as_view()),
-    path('cars_list/', CarList.as_view()),
+    path('cars/', CarList.as_view()),
+    path('cars/<int:pk>/', CarDetail.as_view()),
 ])
--- car/views.py
+++ car/views.py
@@ -1,3 +1,36 @@
+'''
+from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework import mixins
+from rest_framework import generics
+
+
+class CarList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class CarDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+'''
 from django.http import Http404
 from car.serializers import CarSerializer
 from car.models import Car
@@ -46,5 +79,3 @@ class CarList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
