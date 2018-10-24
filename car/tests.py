8ead6db500923eef2578aaef78cb0cb487b6a77f
Artemion <artemdanilenko1997@gmail.com>
2018-10-24 13:52:03 +0300

    check my tests logic

--- car/tests.py
+++ car/tests.py
@@ -1,49 +1,36 @@
 from django.test import TestCase
 from car.models import Car
-from django.test import Client
+from car.models import validate_latitude, validate_longtitude
 
 
-class CarTestCase(TestCase):
+class CarModelTestCase(TestCase):
 
     def setUp(self):
-        Car.objects.create(owner_id=1, brand_id=1, price=1470, color='YELLOW', transmission_id=2)
-        Car.objects.create(owner_id=1, brand_id=2, price=2470, color='BLACK', transmission_id=1)
+        Car.objects.create(owner_id=1, brand_id=1, price=1470, color='YELLOW', transmission_id=1)
+        Car.objects.create(owner_id=2, brand_id=2, price=2470, color='BLACK', transmission_id=2)
+        Car.objects.create(owner_id=3, brand_id=3, price=3470, color='GREEN', transmission_id=3)
 
-    def test_main_endpoint(self):
-        client = Client()
-        response = client.get('/cars/')
-        print(response.content)
+    def test_get_count_fields(self):
+        self.assertEqual(Car.objects.count(), 3)
 
-# from django.test import TestCase
-# from car.models import Car
-# from django.core.exceptions import ValidationError
-# from django.test.client import Client
-#
-# # Create your tests here.
-# from car.models import validate_longtitude
-#
-#
-# class TestValidators(TestCase):
-#     '''
-#     def test_validate_longtitude(self):
-#         valid_long = [89.01, 0.00, -89.9]
-#         for val in valid_long:
-#             validate_longtitude(val)
-#
-#     def test_not_valide_longtitude(self):
-#         not_valid_long = [91.00, 180.00, 152.06, -105.01, -90.01]
-#         for val in not_valid_long:
-#             with self.assertRaises(Exception):
-#                 validate_longtitude(val)
-#     '''
-#
-#
-# class TestValidators(Client):
-#
-#     def get(self, path, data=None, follow=False, secure=False, **extra):
-#
-#
-# # def test_main_endpoint(self):
-# #    Car.objects.create(owner_id=1, brand_id=1, price=1470, color='YELLOW', transmission_id=2)
-# #    client = Client()
-# #    response = client.get('/cars/')
+
+class TestValidatorsTestCase(TestCase):
+
+    def setUp(self):
+        self.valid_long = [89.01, 0.00, -89.9]
+        self.not_valid_long = [91.00, 180.00, 152.06, -105.01, -90.01]
+
+    def test_validate_longtitude(self):
+        for val in self.valid_long:
+            validate_longtitude(val)
+
+    def test_not_valide_longtitude(self):
+        for val in self.not_valid_long:
+            with self.assertRaises(Exception):
+                validate_longtitude(val)
+
+
+class EndPointTestCase(CarModelTestCase):
+
+    def test_my_think(self):
+        print('here', Car.objects.count())
